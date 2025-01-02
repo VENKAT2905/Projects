ad96549b14aaabc50e721f95f524b770d35d17c5
venkat2905 <venkatchitrakavi@gmail.com>
2025-01-02 21:22:04 +0530

    add SpeedInsight

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { onAuthStateChanged } from 'firebase/auth'
-import { useEffect } from 'react'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Routes, useNavigate } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { auth } from './firebase'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Player from './pages/Player/Player'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 
 const App = () => {
 
@@ -35,7 +35,7 @@ const App = () => {
         <Route path='/login' element={<Login/>}/>
         <Route path='/player/:id'element={<Player/>}/>
       </Routes>
-
+      <SpeedInsight/>
     </div>
   )
 }
